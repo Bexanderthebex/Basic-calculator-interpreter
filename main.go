7bbb4df85d165c7ef621b298faec0e92e670d239
Melbex De Leon <melbex11@gmail.com>
2021-04-06 11:29:51 +0800

    fully working solution

--- main.go
+++ main.go
@@ -7,20 +7,11 @@ import (
 
 func main() {
 	// TODO: create a input over here
-	//stringInput := "(1+(4+5+2)-3)+(6+8)"
-	stringInput := "(123+2)"
+	stringInput := "(123+(2+3)+2)"
 	result := calculate(stringInput)
 	fmt.Println(result)
 }
 
-// case 1: incoming input is a delimiter
-// case 2: incoming input is not a delimiter
-// case 2.a: TOS is an operation therefore iterate backwards until you find a delimiter then perform the operation
-// case 2.b: TOS is a number therefore just insert
-// case 2.c: TOS is a opening parenthesis just insert
-// case 3: incomping input is a white space
-
-// assumption is that the string numbers are already evaluated to a single element
 func evaluate(expressionStack []string) int {
 	subExpressionStack := make([]string, 0, len(expressionStack))
 	var total int
@@ -55,19 +46,18 @@ func evaluate(expressionStack []string) int {
 	return total
 }
 
-// TODO: make sure that the numbers are cramped into one memory space
 func calculate(expression string) int {
 	calculatorStack := make([]string, 0, len(expression))
 	var currentExpressionStart int
 	var currentTotal int
-	for index, s := range expression {
+	for _, s := range expression {
 		incomingValue := string(s)
 		if isWhiteSpace(incomingValue) {
 			continue
 		}
 
 		if isOpeningParenthesis(incomingValue) {
-			currentExpressionStart = index
+			currentExpressionStart = len(calculatorStack)
 		}
 
 		if !isClosingParenthesis(incomingValue) {
@@ -91,10 +81,10 @@ func calculate(expression string) int {
 		if isClosingParenthesis(incomingValue) {
 			calculatorStack = append(calculatorStack, incomingValue)
 
-			subExpressionValue := evaluate(calculatorStack[currentExpressionStart : index+1])
-			currentTotal += subExpressionValue
+			subExpressionValue := evaluate(calculatorStack[currentExpressionStart:])
+			currentTotal = subExpressionValue
 			calculatorStack[currentExpressionStart] = strconv.Itoa(subExpressionValue)
-			calculatorStack = calculatorStack[0:currentExpressionStart]
+			calculatorStack = calculatorStack[0 : currentExpressionStart+1]
 			currentExpressionStart = 0
 			continue
 		}
